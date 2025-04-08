e12e0a787450327847e827d97b299b20b0cb7bca
Milan Jaksic <jaksicmilan26@gmail.com>
2025-04-08 21:48:00 +0200

    Unapredjenje kontrolne table admina

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -2,7 +2,25 @@
 
 require_once 'config.php';
 
-echo "Usao si na platformu! <br>";
+if(!isset($_SESSION['admin_id'])) {
+    header('location: index.php');
+    exit();
+}
 
-var_dump($_SESSION);
+?>
 
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Admin dashboard</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
+    </head>
+    <body>
+        
+<div class="container">
+
+</div>
+
+    </body>
+</html>
