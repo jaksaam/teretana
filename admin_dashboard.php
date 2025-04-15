155a06b1bc22ef073681772a46f4c147239d834e
Milan Jaksic <jaksicmilan26@gmail.com>
2025-04-15 20:59:26 +0200

    Napravljena i istestirana forma za dodavanje clanova.

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -19,7 +19,27 @@ if(!isset($_SESSION['admin_id'])) {
     <body>
         
 <div class="container">
+    <div class="row mb-5">
+        <div class="col-md-6">
+            <h2>Register Member</h2>
+            <form action="register_member.php" method="post" enctype="multipart/from-data">  
+                First Name: <input class="form-control" type="text" name="first_name"> <br>
+                Last Name: <input class ="form-control" type="text" name="last_name">  <br>
+                Email: <input class ="form-control" type="email" name="email">  <br>
+                Phone Number: <input class ="form-control" type="text" name="phone_number">  <br>
+                Training plan:
+                <select class="form-control" name="training_plan_id">
+                    <option value="" disabled selected> Training Plan</option>
+                    <option value="1"> 12 session plan</option>
+                    <option value="2"> 30 session plan</option>
+                    </select><br>
+                    <input type="hidden" name="photo_path" id="photoPathInput">
+                    <div id="dropzone-upload" class="dropzone"></div>
 
+                    <input class="btn btn-primary mt-3" type="submit" value="Register Member">
+            </form>         
+        </div>
+    </div>
 </div>
 
     </body>
