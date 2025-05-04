d8485996c8c65f3fd3a086263c31f363151c9b8f
Milan Jaksic <jaksicmilan26@gmail.com>
2025-05-05 00:45:19 +0200

    Pozatvarane konekcije sa bazom.

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
     $run->execute();  //izvrsenje
 
     $results = $run->get_result();
+
     
     if($results->num_rows == 1) {
        
@@ -21,15 +22,21 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
 
         if(password_verify($password, $admin['password'])) {                  //$admin['password'] === $password pre je ovo bilo u zagradi dok nije dosao momenat za hashovanje sifre.
             $_SESSION['admin_id'] = $admin['admin_id'];    // ovde smo u sesiji sacuvali admin_id, to je zbog zastite ulaska na admin_dashboard i ovo pored je admin koji smo koristili gore vec tu su nam i sifra i sve.
+           
+            $conn->close();
             header('location: admin_dashboard.php');            // ovaj header nam sluzi kao redirect i ovde smo izmenili tacna sifra i namestili da nas vuce ka admin dashboard-u(kontronoj tabli gde se sve odvija sa nasom aplikacijom u smislu dodvanja trenera clanovima itd...)
         } else {
             $_SESSION['error'] = "Netacna lozinka";         //varijabla koja postoji u php $_SESSION i pisali smo tu da nam izbaci poruku nakon pogresnog unosa zavisi sta je u pitanju. Ovde je slucaj sa lozinkom.
+           
+            $conn->close();
             header('location: index.php');   // ovaj header nam sluzi kao redirect i ovde smo izmenili sifra netacna da nam ustvari vuce nazad na stranicu kad pogresimo.
             exit();  //obavezno pisati nakon redirecta da se ne bi mesalo izvrsavanje koje smo dole pisali pa da nam na primer ne obrise poruku odma.
         }
 
     } else {
         $_SESSION['error'] = "Netacan username";     //varijabla koja postoji u php $_SESSION i pisali smo tu da nam izbaci poruku nakon pogresnog unosa zavisi sta je u pitanju. Ovde je slucaj sa username.
+       
+        $conn->close();
         header('location: index.php');      // ovaj header nam sluzi kao redirect i ovde smo izmenili admin(username) ne postoji netacna da nam ustvari vuce nazad na stranicu kad pogresimo.
         exit(); //obavezno pisati nakon redirecta da se ne bi mesalo izvrsavanje koje smo dole pisali pa da nam na primer ne obrise poruku odma.
     }
