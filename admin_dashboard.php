a1db86123e19208e14c8bf47f6a2560b1209e5aa
Milan Jaksic <jaksicmilan26@gmail.com>
2025-05-14 18:45:12 +0200

    Napravljena tabela za ispis svih clanova teretane

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -30,6 +30,85 @@ if(!isset($_SESSION['admin_id'])) {
 <?php endif; ?>
         
 <div class="container">
+
+    <div class="row">
+        <div class="col-md-12">
+
+            <h2>Members List</h2>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Phone Number</th>
+                        <th>Trainer</th>
+                        <th>Photo</th>
+                        <th>Training Plan</th>
+                        <th>Access card</th>
+                        <th>Created at</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <?php
+                        $sql = "SELECT members.*, training_plans.name AS training_plan_name, trainers.first_name AS trainer_first_name, trainers.last_name AS trainer_last_name FROM `members` LEFT JOIN `training_plans` ON members.training_plan_id = training_plans.plan_id LEFT JOIN `trainers` ON members.trainer_id = trainers.trainer_id;";
+
+                        $run = $conn->query($sql);
+
+                        $results = $run->fetch_all(MYSQLI_ASSOC);
+
+           
+                    foreach($results as $result) : ?> 
+
+                        <tr>
+                            <td><?php echo $result['first_name']; ?></td>
+                            <td><?php echo $result['last_name']; ?></td>
+                            <td><?php echo $result['email']; ?></td>
+                            <td><?php echo $result['phone_number']; ?></td>
+                            <td><?php 
+                            
+                            if($result['trainer_first_name']) {
+                                echo $result['trainer_first_name'] . " " . $result['trainer_last_name'];
+                            } else {
+                                echo "Nema trenera";
+                            }
+                            
+                            ?></td>
+                            <td><img style="width: 60px;" src="<?php echo $result['photo_path']; ?>"></td>
+                            <td><?php echo $result['training_plan_name']; ?></td>                     
+                            <td><a target="_blank" href="<?php echo $result['access_card_pdf_path']; ?>">Access Card</a></td>
+                            <td><?php 
+                            
+                            $created_at = strtotime($result['created_at']);
+                            $new_date = date("F, jS, Y", $created_at);
+                            echo $new_date;
+
+                            ?></td>
+                            
+                            <td>
+
+                                <form action="delete_member.php" method="POST">
+                                <input type="hidden" name="member_id" value="<?php echo $result['member_id']; ?>">
+                                <button type="submit">Delete</button>
+                                </form>
+                            
+                            </td>
+                            
+                        </tr>
+
+
+                    <?php endforeach;  ?>
+
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+
+
     <div class="row mb-5">
         <div class="col-md-6">
             <h2>Register Member</h2>
